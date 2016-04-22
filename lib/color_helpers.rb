8b9aebe3d131b25d46d1c03acad2f7d64b06d69f
mschneider85 <mschneider85@gmx.de>
2016-04-23 01:32:17 +0200

    rearanged scaffold

--- lib/color_helpers.rb
+++ lib/color_helpers.rb
@@ -1,7 +1,2 @@
 require 'color_helpers/version'
-require 'color_helpers/action_controller/filters'
-require 'color_helpers/action_view/helpers'
-require 'color_helpers/railtie' if defined?(Rails)
-
-module ColorHelpers
-end
+require 'color_helpers/helpers'
