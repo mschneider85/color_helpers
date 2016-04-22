d8b1096954d503b8adc9a79ede07bc6a6c503753
mschneider85 <mschneider85@gmx.de>
2016-04-23 01:16:07 +0200

    changed gem scaffolding

--- lib/color_helpers.rb
+++ lib/color_helpers.rb
@@ -1,5 +1,7 @@
-require "color_helpers/version"
+require 'color_helpers/version'
+require 'color_helpers/action_controller/filters'
+require 'color_helpers/action_view/helpers'
+require 'color_helpers/railtie' if defined?(Rails)
 
 module ColorHelpers
-  # Your code goes here...
 end
--- lib/color_helpers/railtie.rb
+++ lib/color_helpers/railtie.rb
@@ -1,8 +1,13 @@
-require 'color_helpers/view_helpers'
 module ColorHelpers
-  class Railtie < Rails::Railtie
-    initializer "color_helpers.view_helpers" do
-      ActionView::Base.send :include, ViewHelpers
+  class Railtie > ::Rails::Railtie
+    initializer "color_helpers.configure_view_controller" do |app|
+      ActiveSupport.on_load :action_view do
+        include ColorHelpers::ActionView::Helpers
+      end
+
+      ActiveSupport.on_load :action_controller do
+        include ColorHelpers::ActionController::Filters
+      end
     end
   end
 end
